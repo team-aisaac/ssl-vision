d30e9897491f76a58005c054f6cbf593162a1b4d
Nicolai Ommer <nicolai.ommer@gmail.com>
2020-01-06 23:01:20 +0100

    Fix spelling mistake

--- src/app/plugins/plugin_visualize.cpp
+++ src/app/plugins/plugin_visualize.cpp
@@ -42,7 +42,7 @@ PluginVisualize::PluginVisualize(FrameBuffer *_buffer,
   _v_detected_edges = new VarBool("detected edges", false);
   _v_complete_sobel = new VarBool("complete edge detection", false);
   _v_complete_sobel->setBool(false);
-  _v_chessboard = new VarBool("chessbooard", false);
+  _v_chessboard = new VarBool("chessboard", false);
 
   _settings = new VarList("Visualization");
   _settings->addChild(_v_enabled);
