5e940e850d4ba38132fe530598ad18457ef02335
Nicolai Ommer <nicolai.ommer@gmail.com>
2020-11-28 12:08:46 +0100

    Add debug output

--- src/shared/util/TimeSync.cpp
+++ src/shared/util/TimeSync.cpp
@@ -1,7 +1,7 @@
 #include "TimeSync.h"
 
 const int BUFFER_SIZE = 30;
-const uint64_t MAX_AVG_DIFF = 3e8L; // 300ms
+const uint64_t MAX_AVG_DIFF = 3e8L;  // 300ms
 const uint64_t SYNC_ACCURACY = 1e6L; // 1ms
 
 TimeSync::TimeSync() { currentOffset = 0; }
@@ -20,32 +20,39 @@ void TimeSync::update(uint64_t timestamp) {
 
   if ((avgDiff > MAX_AVG_DIFF) || !offsetBuffer.empty()) {
     // Start syncing
+    if (offsetBuffer.empty()) {
+      std::cout << "Start syning with system clock" << std::endl;
+    }
     offsetBuffer.push_back(timestamp - tRef);
-    if(offsetBuffer.size() > BUFFER_SIZE)
+    if (offsetBuffer.size() > BUFFER_SIZE)
       offsetBuffer.pop_front();
     currentOffset = average(offsetBuffer);
     if (avgDiff < SYNC_ACCURACY) {
       // Converged, stop syncing with reference clock
-      std::cout << "Synced with System clock. offset=" << currentOffset
+      std::cout << "Synced with system clock. offset=" << currentOffset
                 << "ns diff=" << avgDiff << "ns" << std::endl;
       offsetBuffer.clear();
     }
   }
 }
 
-uint64_t TimeSync::sync(uint64_t timestamp) const { return timestamp - currentOffset; }
+uint64_t TimeSync::sync(uint64_t timestamp) const {
+  return timestamp - currentOffset;
+}
 
 uint64_t TimeSync::reverseSync(uint64_t timestamp) const {
   return timestamp + currentOffset;
 }
 
-int64_t TimeSync::calcOffset(uint64_t tRef, uint64_t tOther) { return tOther - tRef; }
+int64_t TimeSync::calcOffset(uint64_t tRef, uint64_t tOther) {
+  return tOther - tRef;
+}
 
 uint64_t TimeSync::average(const std::deque<int64_t> &deque) {
   int size = deque.size();
   double avg = 0;
   for (auto l : deque) {
-    avg += (double) l / size;
+    avg += (double)l / size;
   }
-  return (uint64_t) fabs(avg);
+  return (uint64_t)fabs(avg);
 }
